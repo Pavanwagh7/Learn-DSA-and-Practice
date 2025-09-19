cb3ec1906b74d2e90ef73c907e230200763c0a61
PAVAN SHANTILAL WAGH <pawan.s.wagh@gmail.com>
2025-09-19 22:10:09 +0530

    skip app not apple

--- Recursion/Strings_Subsets_Subsequences/Problem02.java
+++ Recursion/Strings_Subsets_Subsequences/Problem02.java
@@ -3,9 +3,11 @@ package Recursion.Strings_Subsets_Subsequences;
 public class Problem02 {
     public static void main(String[] args) {
         Problem02 p2 = new Problem02();
+        //System.out.println(p2.skip_apple("dcappleiwuapplefb"));
 
-        System.out.print(p2.skip_apple("Paapplevan"));
+        System.out.print(p2.skip_appNot_apple("Paapplvan"));
     }
+    //skip apple
     String skip_apple(String up) {
         if (up.isEmpty()) {
             return "";
@@ -15,5 +17,17 @@ public class Problem02 {
             return skip_apple(up.substring(5));
         else
             return up.charAt(0) + skip_apple(up.substring(1));
+    }
+
+    //skip app when it is not apple
+    String skip_appNot_apple(String up) {
+        if (up.isEmpty()) {
+            return "";
+        }
+
+        if (up.startsWith("app") && !up.startsWith("apple"))
+            return skip_appNot_apple(up.substring(3));
+        else
+            return up.charAt(0) + skip_appNot_apple(up.substring(1));
     }
 }
