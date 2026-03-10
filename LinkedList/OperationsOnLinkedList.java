c9d89ed0fc36b96c190034d4e31e3bd79926e135
Pavan Wagh <pawan.s.wagh@gmail.com>
2026-03-10 22:51:50 +0530

    Operations on Linked List

---
+++ LinkedList/OperationsOnLinkedList.java
@@ -0,0 +1,54 @@
+package LinkedList;
+
+public class OperationsOnLinkedList {
+    public void deleteANode () {
+
+    }
+    public boolean isTargetPresent (Node head,int target) {
+        Node ptr = head;
+        while (ptr != null){
+            if (ptr.value == target) return true;
+            ptr = ptr.next;
+        }
+        return false;
+    }
+    public int valueAtKthNode (Node head,int k) {
+        /** It can be done using as a well */
+        /**
+
+        // base case
+        if (k == 0) return head.value;
+        // return statment - Recursion here
+        return valueAtKthNode(head.nextNode,k-1);    */
+
+        // using Loop
+        Node ptr = head;
+        for (int i = 0;i < k;i++) {
+            ptr = ptr.next;
+        }
+        return ptr.value;
+    }
+    public void printingManually (Node head) {
+        System.out.println(head.value);
+        System.out.println(head.next.value);
+        System.out.println(head.next.next.value);
+        System.out.println(head.next.next.next.value);
+        System.out.println(head.next.next.next.next.value);
+        System.out.println(head.next.next.next.next.next.value);
+        System.out.println(head.next.next.next.next.next.next.value);
+    }
+    public void printUsingLoops (Node head) {
+        Node ptr = head;
+        while (ptr != null){
+            System.out.println(ptr.value);
+            ptr = ptr.next;
+        }
+        // or
+        /**
+         ptr = fN;
+         for (int i = 0; i < data.length; i++) {
+             System.out.println(ptr.value);
+             ptr = ptr.nextNode;
+         }                                        */
+    }
+}
