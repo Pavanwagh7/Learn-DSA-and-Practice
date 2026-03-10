f908459f6c81e4cfa2bf8504a981aecb4d597417
Pavan Wagh <pawan.s.wagh@gmail.com>
2026-03-10 22:54:33 +0530

    Main.java for liked list is commited

---
+++ LinkedList/Main.java
@@ -0,0 +1,24 @@
+package LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList_Implementation1 l1 = new LinkedList_Implementation1();
+        Node head = l1.Implementation();
+
+
+        OperationsOnLinkedList obj = new OperationsOnLinkedList();
+        /** Printitng all the values of linked list **/
+//        // Way 01 - Manually
+//        obj.printingManually(head);
+//        // Way 02 - Using loops
+//        obj.printUsingLoops(head);
+
+//        // value at Kth node ( k = 0 for 1st node)
+//        System.out.println(obj.valueAtKthNode(head,3));
+
+//        // check target is present in the LinkedList or not
+//        System.out.println(obj.isTargetPresent(head,-1));
+
+
+    }
+}
