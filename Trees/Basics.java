16e7ed78d71e57709ed5ecf3969645aa96492630
Pavan Wagh <pawan.s.wagh@gmail.com>
2026-06-07 00:33:43 +0530

    feat(Trees)-Preorder, Inorder, Postorder Traversal Implemented

--- Trees/Basics.java
+++ Trees/Basics.java
@@ -80,6 +80,33 @@ class BinaryTree {
         
         
     }
+    public void preorder (Node node) {
+        if (node == null) return;
+
+        System.out.println(node.value);
+
+        preorder(node.left);
+
+        preorder(node.right);
+    }
+    public void inorder (Node node) {
+        if (node == null) return;
+
+        inorder(node.left);
+
+        System.out.println(node.value);
+        
+        inorder(node.right);
+    }
+    public void postorder (Node node) {
+        if (node == null) return;
+
+        postorder(node.left);
+        
+        postorder(node.right);
+        
+        System.out.println(node.value);
+    }
 }
 
 public class Basics {
